c8a9911e27696c90203bfcb30ee587265c50348d
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-04-08 16:33:18 +0100

    Add checkout views and template

--- summit_style/settings.py
+++ summit_style/settings.py
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    
+    #Other
+    
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +68,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'summit_style.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -78,8 +84,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', #allauth requires
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
