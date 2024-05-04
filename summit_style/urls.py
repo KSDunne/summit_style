f54342da66dce13322126c5495be708b71ef70d8
KSDunne <77549699+KSDunne@users.noreply.github.com>
2024-05-04 21:29:02 +0100

    Start contact app

--- summit_style/urls.py
+++ summit_style/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('contact/', include('contact.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
