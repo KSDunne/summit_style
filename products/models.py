1d1d90190f50ca4a777ab53eab33b161a6c7c89c
Katie Dunne <katiedunne123@yahoo.ie>
2024-05-20 15:53:37 +0100

    Show the latest reviews at the top

--- products/models.py
+++ products/models.py
@@ -72,7 +72,7 @@ class Star(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.product.name} rated {self.rating} by {self.user}"
