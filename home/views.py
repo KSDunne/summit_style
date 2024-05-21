ca9fbebc10555af8e199e906c47ccdb80b1c2ebc
Katie Dunne <katiedunne123@yahoo.ie>
2024-05-21 14:14:12 +0100

    Add privacy policy content

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def privacy(request):
+    """ A view to return the privacy policy """
+
+    return render(request, 'home/privacy.html')
