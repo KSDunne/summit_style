b8f922488200cc21cc34d78540abefb902e08370
Katie Dunne <katiedunne123@yahoo.ie>
2024-05-24 19:32:17 +0100

    Add docstrings to products models

--- products/models.py
+++ products/models.py
@@ -9,13 +9,23 @@ from django.db.models import Avg
 
 # Models for products app
 
-"""
-Credit: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob
-/5e595d250f0d7a408a7ccd40bfa25d24c000034d/products/models.py#L3
-"""
-
 
 class Category(models.Model):
+    """
+    This is a model representing a product category.
+
+    Fields:
+        - `name` (CharField): Name of the category.
+        - `friendly_name` (CharField): A user-friendly
+        name for the category (optional).
+
+    Methods:
+        - `get_friendly_name()`: Method to retrieve the
+        friendly name of the category.
+
+    Credit: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob
+    /5e595d250f0d7a408a7ccd40bfa25d24c000034d/products/models.py#L3
+    """
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -30,15 +40,33 @@ class Category(models.Model):
         return self.friendly_name
 
 
-"""
-Credit: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob
-/5e595d250f0d7a408a7ccd40bfa25d24c000034d/products/models.py#L18
-I changed this model by adding a 'wishlist' field, a 'is_course' field
-and I set 'has_sizes' to a default of True.
-"""
-
-
 class Product(models.Model):
+    """
+    Model for a product.
+
+    Fields:
+        - `category` (ForeignKey): Reference to the category this product belongs to (optional).
+        - `sku` (CharField): Stock Keeping Unit for the product (optional).
+        - `name` (CharField): Name of the product.
+        - `description` (TextField): Description of the product.
+        - `has_sizes` (BooleanField): Indicates whether the product comes in different sizes (optional).
+        - `is_course` (BooleanField): Indicates whether the product is a course (optional).
+        - `price` (DecimalField): Price of the product.
+        - `image_url` (URLField): URL link to the product image (optional).
+        - `image` (ImageField): Image of the product (optional).
+        - `wishlist` (ManyToManyField): Reference to users who have added this product to their wishlist.
+
+    Methods:
+        - `__str__`: String representation of the product.
+        - `average_rating()`: Calculates the average rating for the product.
+        - `number_of_wishes()`: Counts the number of users who have added this product to their wishlist.
+
+    Credit: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob
+    /5e595d250f0d7a408a7ccd40bfa25d24c000034d/products/models.py#L18
+    I customised this model by adding a 'wishlist' field, a 'is_course' field
+    and I set 'has_sizes' to a default of True.
+    """
+
     category = models.ForeignKey(
         "Category", null=True, blank=True, on_delete=models.SET_NULL
     )
@@ -67,6 +95,27 @@ class Product(models.Model):
 
 
 class Star(models.Model):
+    """
+    Model representing a star rating and review for a product.
+
+    Fields:
+        - `user` (ForeignKey): Reference to the user who submitted the rating and review.
+        - `product` (ForeignKey): Reference to the product being rated and reviewed.
+        - `rating` (IntegerField): Numeric rating given by the user, ranging from 1 to 5.
+        - `title` (CharField): Title of the review.
+        - `body` (TextField): Content of the review.
+        - `created_on` (DateTimeField): Date and time when the review was created.
+        - `updated_on` (DateTimeField): Date and time when the review was last updated.
+
+    Meta:
+        - `ordering`: Ordering for queries based on creation date.
+
+    Methods:
+        - `__str__`: String representation of the rating and review.
+
+    Credit: https://medium.com/geekculture/django-implementing-star-rating-e1deff03bb1c
+    """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.IntegerField(
