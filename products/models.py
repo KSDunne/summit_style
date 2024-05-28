12c132832be9d1b24797e8f8d7e0da55f73febd1
Katie Dunne <katiedunne123@yahoo.ie>
2024-05-28 18:12:59 +0100

    Make changes to products models due to linter

--- products/models.py
+++ products/models.py
@@ -45,21 +45,26 @@ class Product(models.Model):
     Model for a product.
 
     Fields:
-        - `category` (ForeignKey): Reference to the category this product belongs to (optional).
+        - `category` (ForeignKey): Reference to the category this product
+            belongs to (optional).
         - `sku` (CharField): Stock Keeping Unit for the product (optional).
         - `name` (CharField): Name of the product.
         - `description` (TextField): Description of the product.
-        - `has_sizes` (BooleanField): Indicates whether the product comes in different sizes (optional).
-        - `is_course` (BooleanField): Indicates whether the product is a course (optional).
+        - `has_sizes` (BooleanField): Indicates whether the product comes in
+            different sizes (optional).
+        - `is_course` (BooleanField): Indicates whether the product is a course
+            (optional).
         - `price` (DecimalField): Price of the product.
         - `image_url` (URLField): URL link to the product image (optional).
         - `image` (ImageField): Image of the product (optional).
-        - `wishlist` (ManyToManyField): Reference to users who have added this product to their wishlist.
+        - `wishlist` (ManyToManyField): Reference to users who have added this
+            product to their wishlist.
 
     Methods:
         - `__str__`: String representation of the product.
         - `average_rating()`: Calculates the average rating for the product.
-        - `number_of_wishes()`: Counts the number of users who have added this product to their wishlist.
+        - `number_of_wishes()`: Counts the number of users who have added this
+            product to their wishlist.
 
     Credit: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob
     /5e595d250f0d7a408a7ccd40bfa25d24c000034d/products/models.py#L18
@@ -82,16 +87,16 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    wishlist = models.ManyToManyField(User, related_name="product_wishlist", blank=True)
+    wishlist = models.ManyToManyField(User, related_name="product_wishlist",
+                                      blank=True)
 
     def __str__(self):
         return self.name
 
     def average_rating(self) -> int:
         avg_rating = (
-            Star.objects.filter(product=self).aggregate(Avg("rating"))["rating__avg"]
-            or 0
-        )
+            Star.objects.filter(product=self).aggregate(Avg("rating"))
+            ["rating__avg"] or 0)
         return round(avg_rating)
 
     def number_of_wishes(self):
@@ -103,13 +108,18 @@ class Star(models.Model):
     Model representing a star rating and review for a product.
 
     Fields:
-        - `user` (ForeignKey): Reference to the user who submitted the rating and review.
-        - `product` (ForeignKey): Reference to the product being rated and reviewed.
-        - `rating` (IntegerField): Numeric rating given by the user, ranging from 1 to 5.
+        - `user` (ForeignKey): Reference to the user who submitted the
+            rating and review.
+        - `product` (ForeignKey): Reference to the product being rated
+            and reviewed.
+        - `rating` (IntegerField): Numeric rating given by the user, ranging
+            from 1 to 5.
         - `title` (CharField): Title of the review.
         - `body` (TextField): Content of the review.
-        - `created_on` (DateTimeField): Date and time when the review was created.
-        - `updated_on` (DateTimeField): Date and time when the review was last updated.
+        - `created_on` (DateTimeField): Date and time when the review
+            was created.
+        - `updated_on` (DateTimeField): Date and time when the review
+            was last updated.
 
     Meta:
         - `ordering`: Ordering for queries based on creation date.
@@ -117,7 +127,8 @@ class Star(models.Model):
     Methods:
         - `__str__`: String representation of the rating and review.
 
-    Credit: https://medium.com/geekculture/django-implementing-star-rating-e1deff03bb1c
+    Credit: https://medium.com/geekculture/django-implementing-star-
+                rating-e1deff03bb1c
     """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
