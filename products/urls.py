7cdd105993590e46f4938f2d0820b3c07b07d1e2
Katie Dunne <katiedunne123@yahoo.ie>
2024-05-28 18:16:07 +0100

    Make changes to urls file due to linter results

--- products/urls.py
+++ products/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path("<int:product_id>/", views.product_detail, name="product_detail"),
     path("add/", views.add_product, name="add_product"),
     path("edit/<int:product_id>/", views.edit_product, name="edit_product"),
-    path("delete/<int:product_id>/", views.delete_product, name="delete_product"),
+    path("delete/<int:product_id>/", views.delete_product,
+         name="delete_product"),
     path("review_edit/<int:pk>/", EditReview.as_view(), name="edit_review"),
-    path("delete_review/<int:pk>", DeleteReview.as_view(), name="delete_review"),
+    path("delete_review/<int:pk>", DeleteReview.as_view(),
+         name="delete_review"),
     path("wishlist/<int:pk>/", Wishlist.as_view(), name="wishlist"),
     path("my_wishlist/", MyWishlist.as_view(), name="my_wishlist"),
 ]
